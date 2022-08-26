0c652e77261956be39be1b18af5ea7863550b15a
Manik Rana <manikrana54@gmail.com>
2022-08-26 20:22:50 +0530

    Create REST API's endpoints

---
+++ server/models/user_models.go
@@ -0,0 +1,15 @@
+package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type User struct {
+	Id       primitive.ObjectID `json:"id,omitempty"`
+    Name     string             `json:"name,omitempty" validate:"required"`
+    Location string             `json:"location,omitempty" validate:"required"`
+    Title    string             `json:"title,omitempty" validate:"required"`
+}
+
+// Create a User struct with required properties. 
+// We added omitempty and validate:"required"
+// to the struct tag to tell Fiber to ignore
+// empty fields and make the field required, respectively
